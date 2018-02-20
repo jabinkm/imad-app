2142076827124b278bad410a093fe965ac92cdfc
jabinkm <36657358+jabinkm@users.noreply.github.com>
2018-02-20 02:37:07 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '27091513554_2e6acdd6d7_b.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.pn'));
 });
 
 
