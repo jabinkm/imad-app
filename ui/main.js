01bd2381461b9f460646504700405b48348ff649
jabinkm <36657358+jabinkm@users.noreply.github.com>
2018-02-27 08:10:32 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 var img = document.getElementById('hacker_cyber_crime-512');
 img.onclick = function () {
-  var interval = setInterval(move, 100);
+  var interval = setInterval(moveRight, 100);
 };
