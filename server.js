72ad6bc222f60f6f2d0e9711f5c7cb9f842d1e4f
jabinkm <36657358+jabinkm@users.noreply.github.com>
2018-02-27 07:23:13 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
-  res.send(createTemplate(Article[articleName]));
+  res.send(createTemplate(Articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
