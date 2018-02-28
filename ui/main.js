3c96164d613f00d18054a94db0a896aa70b5462d
jabinkm <36657358+jabinkm@users.noreply.github.com>
2018-02-27 21:29:28 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,10 +14,21 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
+    //make a request to the counter endpoint
+    var request = new XMLHttpRequest();
     
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
+    //counter the response and store it in a variable
+    request.onreadystatechange = function () {
+       if(request.readyState === XMLHttpRequest.DONE) {
+           if(request.status === 200) {
+              var counter = request.responseText; 
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+       }
+        
+    }
+    };
+    //render the variable in the correct span
+    request.open('GET', 'http://jabinkm007.imad.hasura-app.io/counter', true);
+    request.sent(null);
 };
