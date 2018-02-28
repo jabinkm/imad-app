206d45e952aa41587b7f3f4bbd369151150a69c3
jabinkm <36657358+jabinkm@users.noreply.github.com>
2018-02-27 21:11:48 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,23 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New value';
-var img = document.getElementById('hacker_cyber_crime-512');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
+//console.log('Loaded!');
+//var element = document.getElementById('main-text');
+//element.innerHTML = 'New value';
+//var img = document.getElementById('hacker_cyber_crime-512');
+//var marginLeft = 0;
+//function moveRight () {
+//    marginLeft = marginLeft + 5;
+//    img.style.marginLeft = marginLeft + 'px';
+//}
+//img.onclick = function () {
+//  var interval = setInterval(moveRight, 50);
+//};
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+    
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 };
