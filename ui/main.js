8756d7cc73c7cbb1b86a87f0ce69a01db8f72d66
jabinkm <36657358+jabinkm@users.noreply.github.com>
2018-02-27 20:56:47 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ element.innerHTML = 'New value';
 var img = document.getElementById('hacker_cyber_crime-512');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-  var interval = setInterval(moveRight, 100);
+  var interval = setInterval(moveRight, 50);
 };
